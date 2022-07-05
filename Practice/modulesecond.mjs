336f7bc54b30e3a94ad8140e418d5cd122f0f494
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-07-05 23:41:15 +0530

    Add files via upload

---
+++ Practice/modulesecond.mjs
@@ -0,0 +1,12 @@
+export function simple(){
+    console.log("Simple is Complex");
+    return 37;
+}
+
+export default function simple2(){
+// export function simple2(){
+    console.log("Simple2 is Complex");
+}
+
+// module.exports = simple;
+
