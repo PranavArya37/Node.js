d4dbf36c47f4e75ddce455214bba29fe288ff2f0
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-07-05 23:41:05 +0530

    Add files via upload

---
+++ Practice/modulefirst.js
@@ -0,0 +1,12 @@
+// const simple2 = require('./modulesecond');
+
+// import simple from "./modulesecond.mjs"
+// import {simple, simple2} from "./modulesecond.mjs"
+// import {simple, simple2 as hello} from "./modulesecond.mjs"
+import * as a1 from "./modulesecond.mjs"
+
+// simple();
+// simple2();
+console.log(a1.simple());
+
+// hello();
