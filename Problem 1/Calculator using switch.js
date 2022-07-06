01f326f01c1dfcc9653b3c4c1fa1eca12346a08b
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-07-07 03:29:43 +0530

    Add files via upload

---
+++ Problem 1/Calculator using switch.js
@@ -0,0 +1,39 @@
+// Calculator using switch
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using switch
+switch(operator) {
+    case '+':
+         result = number1 + number2;
+        console.log(`${number1} + ${number2} = ${result}`);
+        break;
+
+    case '-':
+         result = number1 - number2;
+        console.log(`${number1} - ${number2} = ${result}`);
+        break;
+
+    case '*':
+         result = number1 * number2;
+        console.log(`${number1} * ${number2} = ${result}`);
+        break;
+
+    case '/':
+         result = number1 / number2;
+        console.log(`${number1} / ${number2} = ${result}`);
+        break;
+
+    default:
+        console.log('Invalid operator');
+        break;
+}
