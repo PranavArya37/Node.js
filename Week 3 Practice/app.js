398e4b65bc17c711e60201c85eccce0a5bc18b03
Pranav Arya <43077165+PranavArya37@users.noreply.github.com>
2022-07-27 01:21:39 +0530

    Add files via upload

---
+++ Week 3 Practice/app.js
@@ -0,0 +1,12 @@
+// module.exports = function(){
+//     return 'hello';
+// }
+
+module.exports = {
+    sayHello: function(){
+        return 'hello';
+    },
+    addNumbers: function(value1, value2){
+        return value1 + value2;
+    }
+}
